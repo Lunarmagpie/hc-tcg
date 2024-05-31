ec4941239759d3c2f3e1485a38c53b041dd788f0
Benji42 <63879236+Benji42@users.noreply.github.com>
2024-05-31 16:47:31 -0400

    push

--- common/cards/base/hermit-card.ts
+++ common/cards/base/hermit-card.ts
@@ -66,52 +66,6 @@ export const hermitCardDefaults = {
 	sidebarDescriptions: [],
 }
 
-// Default is to return
-function createAttackModel(
-	game: GameModel,
-	hermit: Card & HermitAttack,
-	pos: CardPosModel,
-	hermitAttackType: HermitAttackType
-): AttackModel | null {
-	if (pos.rowIndex === null || !pos.row || !pos.row.hermitCard) return null
-
-	const {opponentPlayer: opponentPlayer} = game
-	const targetIndex = opponentPlayer.board.activeRow
-	if (targetIndex === null) return null
-
-	const targetRow = opponentPlayer.board.rows[targetIndex]
-	if (!targetRow.hermitCard) return null
-
-	// Create an attack with default damage
-	const attack = new AttackModel({
-		creator: hermit,
-		attacker: {
-			player: pos.player,
-			rowIndex: pos.rowIndex,
-			row: pos.row,
-		},
-		target: {
-			player: opponentPlayer,
-			rowIndex: targetIndex,
-			row: targetRow,
-		},
-		type: hermitAttackType,
-		createWeakness: 'ifWeak',
-		log: (values) =>
-			`${values.attacker} ${values.coinFlip ? values.coinFlip + ', then ' : ''} attacked ${
-				values.target
-			} with ${values.attackName} for ${values.damage} damage`,
-	})
-
-	if (hermitAttackType == 'primary') {
-		attack.addDamage(hermit, hermit.primary.damage)
-	} else if (hermitAttackType == 'secondary') {
-		attack.addDamage(hermit, hermit.secondary.damage)
-	}
-
-	return attack
-}
-
 function getActions(game: GameModel): TurnActions {
 	const {currentPlayer} = game
 
@@ -120,13 +74,6 @@ function getActions(game: GameModel): TurnActions {
 
 	return spaceForHermit ? ['PLAY_HERMIT_CARD'] : []
 }
-
-// export function getFormattedDescription(): FormattedTextNode {
-// 	return formatText(
-// 		(this.primary.power ? `**${this.primary.name}**\n*${this.primary.power}*` : '') +
-// 			(this.secondary.power ? `**${this.secondary.name}**\n*${this.secondary.power}*` : '')
-// 	)
-// }
 
 export function getHermitCardDefaults(name: string) {
 	return {
