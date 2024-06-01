7ea4471a58006b1ae6eafbd85663c90f5be454e8
Benji42 <63879236+Benji42@users.noreply.github.com>
2024-05-31 21:20:02 -0400

    combinators

--- common/cards/base/attachable.ts
+++ common/cards/base/attachable.ts
@@ -1,52 +1,52 @@
-import { CardPosModel } from "../../models/card-pos-model";
-import { GameModel } from "../../models/game-model";
+import {CardPosModel} from '../../models/card-pos-model'
+import {GameModel} from '../../models/game-model'
 
 type attachmentCombinator = (game: GameModel, pos: CardPosModel) => boolean
 
 export namespace combinators {
-  export function or(...options: Array<attachmentCombinator>): attachmentCombinator {
-    return (game, pos) => {
-      return options.reduce(
-        (place, combinator) => place || combinator(game, pos),
-        false
-      )
-    }
-  }
+	export function or(...options: Array<attachmentCombinator>): attachmentCombinator {
+		return (game, pos) => {
+			return options.reduce((place, combinator) => place || combinator(game, pos), false)
+		}
+	}
 
-  export function and(...options: Array<attachmentCombinator>): attachmentCombinator {
-    return (game, pos) => {
-      return options.reduce(
-        (place, combinator) => place && combinator(game, pos),
-        true
-      )
-    }
-  }
+	export function and(...options: Array<attachmentCombinator>): attachmentCombinator {
+		return (game, pos) => {
+			return options.reduce((place, combinator) => place && combinator(game, pos), true)
+		}
+	}
 
-
-  export const player: attachmentCombinator = (game, pos) {
-
-  }
-  export const opponent: attachmentCombinator = (game, pos) {
-
-  }
-  export const hermit: attachmentCombinator = (game, pos) {
-    
-  }
-  export const effect: attachmentCombinator = (game, pos) {
-    
-  }
-
+	export const player: attachmentCombinator = (game, pos) => {
+		if (pos.player === game.currentPlayer) return true
+		return false
+	}
+	export const opponent: attachmentCombinator = (game, pos) => {
+		if (pos.player === game.opponentPlayer) return true
+		return false
+	}
+	export const hermit: attachmentCombinator = (game, pos) => {
+		if (pos.slot.type === 'hermit') return true
+		return false
+	}
+	export const effect: attachmentCombinator = (game, pos) => {
+		if (pos.slot.type === 'effect') return true
+		return false
+	}
+	export const singleUse: attachmentCombinator = (game, pos) => {
+		if (pos.slot.type === 'single_use') return true
+		return false
+	}
+	export const item: attachmentCombinator = (game, pos) => {
+		if (pos.slot.type === 'item') return true
+		return false
+	}
 }
-
 
 export interface IsAttachable {
-  __is_attachable: undefined,
-  canBeAttachedTo: attachmentCombinator;
+	__is_attachable: undefined
+	canBeAttachedTo: attachmentCombinator
 }
-export const isAttachableToHermitSlotsDefaults = { __is_attachable_to_hermit_slots: undefined }
+export const isAttachableToHermitSlotsDefaults = {__is_attachable_to_hermit_slots: undefined}
 export function implementsIsAttachableToHermitSlots(obj: any): obj is IsAttachable {
-  return '__is_attachable' in obj
+	return '__is_attachable' in obj
 }
-
-
-
