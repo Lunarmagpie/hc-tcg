223d722db6973a7c4b60746f67815788db513af1
cheezyanimations@gmail.com <cheezyanimations@gmail.com>
2024-06-07 21:29:56 +0000

    mend

--- common/cards/base/card.ts
+++ common/cards/base/card.ts
@@ -74,6 +74,26 @@ export abstract class Card<T extends CardProps = CardProps> {
 	implementsGetAttack(this: any): this is Card<T> & GetAttack {
 		return 'getAttack' in this
 	}
+	
+	/** Updates the log entry*/
+	updateLog(this: Card<T>, logEntry: (values: PlayCardLog) => string) {
+		this.props.log.push(logEntry)
+	}
+
+	private consolidateLogs(this: Card<T>, values: PlayCardLog, logIndex: number) {
+		if (logIndex > 0) {
+			values.previousLog = this.consolidateLogs(values, logIndex - 1)
+		}
+		return this.props.log[logIndex](values)
+	}
+
+	/** Gets the log entry for this attack*/
+	getLog(this: Card<T>, values: PlayCardLog) {
+		if (this.props.log.length === 0) {
+			return ''
+		}
+		return this.consolidateLogs(values, this.props.log.length - 1)
+	}
 }
 
 export interface CardProps {
@@ -97,7 +117,7 @@ export interface CardProps {
 	canBeAttachedTo: AttachmentExpression
 
 	//@TODO remove this and make mixin
-	log: ((values: PlayCardLog) => string) | null
+	log: Array<(values: PlayCardLog) => string>
 }
 export const isCardDefaults = {__card: undefined}
 
@@ -134,91 +154,8 @@ export const hasBattleLogDefaults = {
 	__has_battle_log: undefined,
 }
 
-<<<<<<< HEAD
 export interface IsSingleUse {}
 export const isSingleUseDefaults = {__is_single_use: undefined}
-=======
-	/** The battle log attached to this card */
-	/** Set to string when the card should generate a log when played or applied, and null otherwise */
-	private log: Array<(values: PlayCardLog) => string>
-
-	constructor(defs: CardDefs) {
-		this.type = defs.type
-		this.id = defs.id
-		this.numericId = defs.numericId
-		this.name = defs.name
-		this.rarity = defs.rarity
-		this.log = []
-	}
-
-	public getKey(keyName: string) {
-		return this.id + ':' + keyName
-	}
-	public getInstanceKey(instance: string, keyName: string = '') {
-		return this.id + ':' + instance + ':' + keyName
-	}
-
-	/**
-	 * If the specified slot is empty, can this card be attached there
-	 *
-	 * Returns an array of any of the problems there are with attaching, if any
-	 */
-	public abstract canAttach(game: GameModel, pos: CardPosModel): CanAttachResult
-
-	/**
-	 * Returns the description for this card that shows up in the sidebar.
-	 */
-	public abstract getFormattedDescription(): FormattedTextNode
-
-	/**
-	 * Called when an instance of this card is attached to the board
-	 */
-	public onAttach(game: GameModel, instance: string, pos: CardPosModel) {
-		// default is do nothing
-	}
-
-	/**
-	 * Called when an instance of this card is removed from the board
-	 */
-	public onDetach(game: GameModel, instance: string, pos: CardPosModel) {
-		// default is do nothing
-	}
-
-	/**
-	 * Returns the expansion this card is a part of
-	 */
-	public getExpansion(): string {
-		return 'default'
-	}
-
-	/**
-	 * Returns the palette to use for this card
-	 */
-	public getPalette(): string {
-		return 'default'
-	}
-
-	/**
-	 * Returns the shortened name to use for this card
-	 */
-	public getShortName(): string {
-		return this.name
-	}
-
-	/**
-	 * Returns whether to show *Attach* on the card tooltip
-	 */
-	public showAttachTooltip(): boolean {
-		return false
-	}
-
-	/**
-	 * Returns whether to show *Single Use* on the card tooltip
-	 */
-	public showSingleUseTooltip(): boolean {
-		return false
-	}
->>>>>>> battle-log-and-attack-previews
 
 export interface HasTurnActions {
 	__has_turn_actions: undefined
@@ -229,38 +166,9 @@ export interface HasTurnActions {
 }
 export const hasTurnActionsDefaults = {__has_turn_actions: undefined}
 
-<<<<<<< HEAD
 export interface HasAttach {
 	onAttach(game: GameModel, pos: CardPosModel): void
 	onDetach(game: GameModel, pos: CardPosModel): void
-=======
-	/**
-	 * Returns the sidebar descriptions for this card
-	 */
-	public sidebarDescriptions(): Array<Record<string, string>> {
-		return []
-	}
-
-	/** Updates the log entry*/
-	public updateLog(logEntry: (values: PlayCardLog) => string) {
-		this.log.push(logEntry)
-	}
-
-	private consolidateLogs(values: PlayCardLog, logIndex: number) {
-		if (logIndex > 0) {
-			values.previousLog = this.consolidateLogs(values, logIndex - 1)
-		}
-		return this.log[logIndex](values)
-	}
-
-	/** Gets the log entry for this attack*/
-	public getLog(values: PlayCardLog) {
-		if (this.log.length === 0) {
-			return ''
-		}
-		return this.consolidateLogs(values, this.log.length - 1)
-	}
->>>>>>> battle-log-and-attack-previews
 }
 
 export interface GivesPointOnKnockout {}
