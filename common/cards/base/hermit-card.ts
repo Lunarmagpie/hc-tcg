61a29b96188d30a1531f3701536cc1a858f154f3
cheezyanimations@gmail.com <cheezyanimations@gmail.com>
2024-06-08 03:04:51 +0000

    fix hermit card

--- common/cards/base/hermit-card.ts
+++ common/cards/base/hermit-card.ts
@@ -18,8 +18,6 @@ import {CardCategoryT, PlayCardLog} from '../../types/cards'
 import {TurnActions} from '../../types/game-state'
 import {formatText} from '../../utils/formatting'
 import attachableTo from './attachable'
-import Card, {CanAttachResult} from './card'
-import {CardRarityT, HermitAttackInfo, HermitTypeT, PlayCardLog} from '../../types/cards'
 import {HermitAttackType} from '../../types/attack'
 import {CardPosModel} from '../../models/card-pos-model'
 import {AttackModel} from '../../models/attack-model'
@@ -57,100 +55,6 @@ export const hermitCardDefaults = {
 	log: (values: PlayCardLog) => `$p{You|${values.player}}$ placed $p${values.pos.name}$`,
 	sidebarDescriptions: [],
 }
-	
-	// To ensure Armor Stand has the same log as HermitCards, this is exportable.
-export function hermitCardBattleLog(name: string) {
-	return (values: PlayCardLog) =>
-		`$p{You|${values.player}}$ placed $p${name}$ on row #${values.pos.rowIndex}`
-}
-
-abstract class HermitCard extends Card {
-	public hermitType: HermitTypeT
-	public health: number
-	public primary: HermitAttackInfo
-	public secondary: HermitAttackInfo
-
-	constructor(defs: HermitDefs) {
-		super({
-			type: 'hermit',
-			id: defs.id,
-			numericId: defs.numericId,
-			name: defs.name,
-			rarity: defs.rarity,
-		})
-
-		this.hermitType = defs.hermitType
-		this.health = defs.health
-		this.primary = defs.primary
-		this.secondary = defs.secondary
-		this.updateLog(hermitCardBattleLog(this.name))
-	}
-
-	public override canAttach(game: GameModel, pos: CardPosModel): CanAttachResult {
-		const {currentPlayer} = game
-
-		const result: CanAttachResult = []
-
-		if (pos.slot.type !== 'hermit') result.push('INVALID_SLOT')
-		if (pos.player.id !== currentPlayer.id) result.push('INVALID_PLAYER')
-
-		return result
-	}
-
-	// Default is to return
-	public getAttack(
-		game: GameModel,
-		instance: string,
-		pos: CardPosModel,
-		hermitAttackType: HermitAttackType
-	): AttackModel | null {
-		if (pos.rowIndex === null || !pos.row || !pos.row.hermitCard) return null
-
-		const {opponentPlayer: opponentPlayer} = game
-		const targetIndex = opponentPlayer.board.activeRow
-		if (targetIndex === null) return null
-
-		const targetRow = opponentPlayer.board.rows[targetIndex]
-		if (!targetRow.hermitCard) return null
-
-		// Create an attack with default damage
-		const attack = new AttackModel({
-			id: this.getInstanceKey(instance),
-			attacker: {
-				player: pos.player,
-				rowIndex: pos.rowIndex,
-				row: pos.row,
-			},
-			target: {
-				player: opponentPlayer,
-				rowIndex: targetIndex,
-				row: targetRow,
-			},
-			type: hermitAttackType,
-			createWeakness: 'ifWeak',
-			log: (values) =>
-				`${values.attacker} ${values.coinFlip ? values.coinFlip + ', then ' : ''} attacked ${
-					values.target
-				} with ${values.attackName} for ${values.damage} damage`,
-		})
-
-		if (attack.type === 'primary') {
-			attack.addDamage(this.id, this.primary.damage)
-		} else if (attack.type === 'secondary') {
-			attack.addDamage(this.id, this.secondary.damage)
-		}
-
-		return attack
-	}
-
-	public override getActions(game: GameModel): TurnActions {
-		const {currentPlayer} = game
-
-		// Is there a hermit slot free on the board
-		const spaceForHermit = currentPlayer.board.rows.some((row) => !row.hermitCard)
-
-		return spaceForHermit ? ['PLAY_HERMIT_CARD'] : []
-	}
 
 function getActions(game: GameModel): TurnActions {
 	const {currentPlayer} = game
