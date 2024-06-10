5d9caf7bf27b002f396e6b471b7c3e3b17afa440
cheezyanimations@gmail.com <cheezyanimations@gmail.com>
2024-06-10 04:28:03 +0000

    health card

--- common/cards/base/health-card.ts
+++ common/cards/base/health-card.ts
@@ -1,30 +1,29 @@
-import {HasHealth, Card, hasHealthDefaults, isCardDefaults} from './card'
+import {HasHealth, hasHealthDefaults, isCardDefaults, CardProps, Card} from './card'
 import {TextNode} from '../../utils/formatting'
 import attachableTo from './attachable'
 
-export type HealthCard = Card & HasHealth
+export type HealthCard = CardProps & HasHealth
 
-export const HealthIndicator = (): HealthCard => {
-	return {
-		...isCardDefaults,
-		...hasHealthDefaults,
-		category: 'health',
-		health: 300,
-		id: 'health',
-		numericId: -1,
-		name: 'Health Card',
-		rarity: 'common',
-		getDescription() {
-			return TextNode('')
-		},
-		expansion: 'default',
-		palette: 'default',
-		sidebarDescriptions: [],
-		log: [],
-	}
-
-	public override getFormattedDescription(): FormattedTextNode {
-		return formatText(`${this.health}`)
+class HealthIndicator extends Card<HealthCard> {
+	constructor() {
+		super({
+			...isCardDefaults,
+			...hasHealthDefaults,
+			category: 'health',
+			health: 300,
+			id: 'health',
+			numericId: -1,
+			canBeAttachedTo: attachableTo.nothing,
+			name: 'Health Card',
+			rarity: 'common',
+			getDescription() {
+				return TextNode('')
+			},
+			expansion: 'default',
+			palette: 'default',
+			sidebarDescriptions: [],
+			log: [],
+		})
 	}
 }
 
